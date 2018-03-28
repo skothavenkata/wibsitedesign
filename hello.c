40ae5059d4803a86ad9b750d7d54280ed6bc6f72
skothavenkata <santosh.reddy563@gmail.com>
2018-03-28 16:39:10 -0600

    This is  second version

---
+++ hello.c
@@ -0,0 +1,4 @@
+hello my name is santosh 
+my project is website design and installation
+thank you
+
---
+++ hello1.c
@@ -0,0 +1,3 @@
+hello my name is santosh 
+my project is website design and i9nstallation for academic
+
